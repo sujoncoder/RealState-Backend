dcc54c8bac78fa4ed213dfbee08a8c952988ba34
sujonphero <sujonsheikh.in@gmail.com>
2024-06-03 20:26:25 +0600

    connectto db

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("DB is connected ❤");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 const app = express();
 
