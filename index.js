12a10625d5eacf5a74330203ecbc6ba377a07cec
sujonphero <sujonsheikh.in@gmail.com>
2024-06-06 00:40:19 +0600

    cors configure

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
 app.listen(PORT, async () => {
   console.log("Server is running on port 3000!");
